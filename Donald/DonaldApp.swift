2df2bdfe912c5684ac45e5cfb4a3bc70ded084f7
katagaki <68231035+katagaki@users.noreply.github.com>
2026-04-06 16:27:55 +0900

    Initial Commit

---
+++ Donald/DonaldApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonaldApp.swift
+//  Donald
+//
+//  Created by シン・ジャスティン on 2026/04/06.
+//
+
+import SwiftUI
+
+@main
+struct DonaldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
